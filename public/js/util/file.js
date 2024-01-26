118f970310c9b807b5ac28120a32c765b74507a3
thanhhien234 <doanthanhhien01@gmail.com>
2024-01-26 23:02:35 +0900

    feat: add modal when uploading file

--- public/js/util/file.js
+++ public/js/util/file.js
@@ -1,10 +1,11 @@
 //select file
 let checkedFileIds = [];
 let firstFilename = null;
+let file= null;
 
 function handleCheckbox(checkbox, fileName) {
     const isChecked = $(checkbox).prop("checked");
-
+    
     if (isChecked) {
         $(checkbox).next('label').css('background-image', 'url(/public/assets/icon/checkBox.png)');
         const fileId = $(checkbox).attr("id");
@@ -40,28 +41,34 @@ $(document).on('click', '#file-remove-button', function() {
 //modify file
 $(document).on('click', '#file-modify-button', function() {
     if (checkedFileIds.length === 1) {
-        const selectedFile = checkedFileIds[0];
         $('#newFileNameInput').val(firstFilename);
         $('#newFileNameInput').css('color', '#929FAD');
-        $('#modifyFileModal').modal('show');
+        $('#modifyFileModal').modal('show')
     } else {
         alert("파일을 하나만 선택해주세요.");
     }
 });
-$(document).on('click', '#newFileNameInput', function() {
-    $(this).css('color', '#001832');
-});
 $(document).on('click', '#saveChangesBtn', function() {
     const newFileName = $('#newFileNameInput').val();
-    modifyFile(checkedFileIds[0], newFileName);
-    $('#modifyFileModal').modal('hide');
-    checkedFileIds = [];
+    if (checkedFileIds.length === 1) {
+        modifyFile(checkedFileIds[0], newFileName);
+        $('#modifyFileModal').modal('hide');
+        checkedFileIds = [];
+    }
+    else{
+        uploadFile(file, newFileName);
+        $('#modifyFileModal').modal('hide');
+    }
+    $('#file-input').val(''); 
+    
 });
 $(document).on('click', '#cancelBtn', function() {
     $('#modifyFileModal').modal('hide');
-});
-
-
+    $('#file-input').val(''); 
+}); 
+$(document).on('click', '#newFileNameInput', function() {
+    $('#newFileNameInput').css('color', '#001832');
+}); 
 
 
 //add file
@@ -70,12 +77,14 @@ $(document).on('click', '#file-add-button', function () {
 });
 $(document).on('change', '#file-input', function () {
     const fileInput = this;
-    const file = fileInput.files[0];
-
+    file = fileInput.files[0];
     if (file) {
         const fileName = file.name;
-        uploadFile(file, fileName);
+        $('#newFileNameInput').val(fileName); 
+        $('#modifyFileModal').modal('show'); 
     } else {
         alert("파일을 선택해주세요.");
     }
 });
+
+
