aba45c5eb91120e1eafe41e0242c3c7303367880
thanhhien234 <doanthanhhien01@gmail.com>
2024-02-12 05:18:08 +0900

    feat: add catergory

--- public/js/api/file/fileListSearch.js
+++ public/js/api/file/fileListSearch.js
@@ -1,18 +1,23 @@
 let currentPage = 0;
-let pageSize = 10;
+let pageSize = 10; //default
 
 async function fileListSearch(page, pageSize) {
+    if (window.innerWidth <= 900) {
+        pageSize = 5;
+    } else {
+        pageSize = 10;
+    }
     await $.ajax({
-        url: config.fileServer + `/api/file/list?page=${page}&pageSize=${pageSize}`,
+        url: config.fileServer + `/api/file/all?page=${page}&pageSize=${pageSize}`,
         type: "GET",
         headers: {
             Authorization: "Bearer " + getCookie("accessToken")
         },
         success: function (res) {
-            const totalPages = res.fileCount / pageSize;
+            const totalPages = totalFiles / pageSize;
             /* large screen */
             $("#fileTable tbody").empty();
-            res.fileGetListElementResList.forEach(function (file, index) {
+            res.forEach(function (file, index) {
                 const formattedDate = new Date(file.created_at).toLocaleDateString("ko-KR", {
                     year: "numeric",
                     month: "long",
@@ -27,6 +32,9 @@ async function fileListSearch(page, pageSize) {
                             <label for="${file.id}"></label>
                         </td>
                         <td>${index + 1 + pageSize * currentPage}</td>
+                        <td>
+                            <span class="category-name" style="background-color: ${categoryColorMap[file.category]}">${file.category}</span>
+                        </td>
                         <td>${file.name}</td>
                         <td>${formattedDate}</td>
                     </tr>
@@ -36,7 +44,7 @@ async function fileListSearch(page, pageSize) {
 
             /* mobile screen */
             $("#mobile-fileTable tbody").empty();
-            res.fileGetListElementResList.forEach(function (file, index) {
+            res.forEach(function (file, index) {
                 const formattedDate = new Date(file.created_at).toLocaleDateString("ko-KR", {
                     year: "numeric",
                     month: "long",
@@ -52,6 +60,9 @@ async function fileListSearch(page, pageSize) {
                         <td>${index + 1 + pageSize * currentPage}</td>
                         <td>
                             <div class="file-info">
+                                <div>
+                                    <span class="category-name" style="background-color: ${categoryColorMap[file.category]}">${file.category}</span>
+                                </div>
                                 <div>${file.name}</div>
                                 <div>${formattedDate}</div>
                             </div>
@@ -60,10 +71,10 @@ async function fileListSearch(page, pageSize) {
                 `);
             });
 
-            let emptyRow = pageSize - res.fileGetListElementResList.length;
+            let emptyRow = pageSize - res.length;
             for (let i = 0; i < emptyRow; i++) {
-                $("#fileTable tbody").append(`<tr><td></td><td></td><td></td><td></td></tr>`);
-                $("#mobile-fileTable tbody").append(`<tr><td></td><td></td><td></td></tr>`);
+                $("#fileTable tbody").append(`<tr><td></td><td></td><td></td><td></td><td></td></tr>`);
+                $("#mobile-fileTable tbody").append(`<tr><td></td><td></td><td></td><td></td></tr>`);
             }
             addPaging(totalPages); // paging
             currentPageButton(); //underline pageButton's text
