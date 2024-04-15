204a23efdf80b98d90b47647ffba798599c2704b
thanhhien234 <doanthanhhien01@gmail.com>
2024-04-15 11:48:08 +0900

    fix: define handleCategoryChange() in header

--- public/js/category/categoryController.js
+++ public/js/category/categoryController.js
@@ -7,10 +7,9 @@ $(document).on('click', '#file-category-button', function() {
 });
  
 /* 저장 button */
-const saveChangesBtn = document.getElementById('categorySaveChangesBtn');
-saveChangesBtn.addEventListener('click', function() {
-    const newCategoryName = document.querySelector('.category-text-input').value.trim();
-    const oldCategoryName = document.getElementById('changeCategorySelect').value;
+$(document).on('click', '#categorySaveChangesBtn', function() {
+    const newCategoryName = $('.category-text-input').val().trim();
+    const oldCategoryName = $('#changeCategorySelect').val();
     if (!newCategoryName || oldCategoryName === "") {
         $('#categoryModal').modal('hide');
         return;
@@ -30,7 +29,7 @@ saveChangesBtn.addEventListener('click', function() {
 
 /* 취소 button*/
 $(document).on('click', '#categoryCancelBtn', function() {
-    const addCategoryInput = document.querySelector('.add-category-input');
+    const addCategoryInput = $('.add-category-input');
     if (addCategoryInput) {
         addCategoryInput.textContent = '카테고리 추가';
     }
@@ -39,7 +38,7 @@ $(document).on('click', '#categoryCancelBtn', function() {
 
 
 
-window.addEventListener('resize', function() {
+$(window).resize(function() {
     if ($(window).width() < 900) { 
         var categorySelectWrapper = $('.category-select-wrapper').detach();
         $('#mobile-fileTable').find('thead').after('<div class="gap"></div>', categorySelectWrapper);
@@ -48,14 +47,4 @@ window.addEventListener('resize', function() {
     }
 });
 
-function handleCategoryChange() {
-    let selectElement = document.getElementById("categorySelect");
-    let selectedOption = selectElement.options[selectElement.selectedIndex];
-    let categoryName = selectedOption.value;
-    if (categoryName === "all") {
-        category.fileListSearch();
-    } else {
-        category.searchCategory(categoryName);
-    }
-}
 
