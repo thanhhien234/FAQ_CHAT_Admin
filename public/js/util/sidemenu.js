33f9e4d31e50cce0ecf4fe8f1cb80f3c537a5431
thanhhien234 <doanthanhhien01@gmail.com>
2024-04-28 02:31:07 +0900

    feat: 초기의 select 위치

--- public/js/util/sidemenu.js
+++ public/js/util/sidemenu.js
@@ -17,6 +17,12 @@ const mobileFileBtnContainer = $(".mobile-file-button-container");
 // Initialize
 $(document).ready(function() {
     $("#answer-menu").click();
+    if ($(window).width() < 900) { 
+        let categorySelectWrapper = $('.category-select-wrapper').detach();
+        $('#mobile-fileTable').find('thead').after('<div class="gap"></div>', categorySelectWrapper);
+    } else {
+        $('.category-select-wrapper').insertBefore('.file-wrapper');
+    }
 });
 
 
